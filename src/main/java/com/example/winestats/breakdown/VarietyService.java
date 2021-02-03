ade00d190dd4bf3561eb53e56fe4311ac61d9b68
Calvin <calvinluy@gmail.com>
2021-02-04 01:11:40 +1100

    sort the output of the breakdown to show highest percentage first

--- src/main/java/com/example/winestats/breakdown/VarietyService.java
+++ src/main/java/com/example/winestats/breakdown/VarietyService.java
@@ -2,12 +2,21 @@ package com.example.winestats.breakdown;
 
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
+import java.util.*;
 
 @Service
 public class VarietyService {
     public String getBreakdownByVariety(String lotCode) {
         HashMap<String, Double> breakdown= new Variety(lotCode).getBreakdown();
-        return breakdown.toString();
+        List<Map.Entry<String, Double>> list = new LinkedList<Map.Entry<String, Double>>(breakdown.entrySet());
+        list.sort(new Comparator<Map.Entry<String, Double>>() {
+            @Override
+            public int compare(Map.Entry<String, Double> o1, Map.Entry<String, Double> o2) {
+                // comparator will sort by descending order of percentage
+                return o2.getValue().compareTo(o1.getValue());
+
+            }
+        });
+        return list.toString();
     }
 }
