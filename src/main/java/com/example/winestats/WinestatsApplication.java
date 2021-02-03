bb1737247a25b4fd686315dcb3feb95e7035faae
Calvin <calvinluy@gmail.com>
2021-02-03 19:24:30 +1100

    created based springboot application

---
+++ src/main/java/com/example/winestats/WinestatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.winestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinestatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/winestats/WinestatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.winestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinestatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
