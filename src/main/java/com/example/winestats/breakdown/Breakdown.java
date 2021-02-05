76fd4710b22a4e0160e2f785198ccc20d4a96e95
Calvin <calvinluy@gmail.com>
2021-02-05 14:24:36 +1100

    added more general class Breakdown to represent all four types

---
+++ src/main/java/com/example/winestats/breakdown/Breakdown.java
@@ -0,0 +1,23 @@
+package com.example.winestats.breakdown;
+
+import java.util.List;
+import java.util.Map;
+
+public class Breakdown {
+
+    private String breakdownType;
+    private List<BreakdownComponent> listComponents;
+
+    public Breakdown(String breakdownType, List<BreakdownComponent> listComponents) {
+        this.breakdownType = breakdownType;
+        this.listComponents = listComponents;
+    }
+
+    public List<BreakdownComponent> getBreakdown() {
+        return listComponents;
+    }
+
+    public String getBreakdownType() {
+        return breakdownType;
+    }
+}
---
+++ src/main/java/com/example/winestats/breakdown/BreakdownComponent.java
@@ -0,0 +1,15 @@
+package com.example.winestats.breakdown;
+
+
+public class BreakdownComponent {
+    private final String key;
+    private final Double percentage;
+
+
+    public BreakdownComponent(String key, Double percentage) {
+        this.key = key;
+        this.percentage = percentage;
+
+    }
+
+}
