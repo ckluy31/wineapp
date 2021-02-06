46c724840302ea7059398392ac14364e4e4805be
Calvin <calvinluy@gmail.com>
2021-02-07 01:03:37 +1100

    added css styling absed on figma to search elements

--- frontend/wine-app/src/search/SearchPage.js
+++ frontend/wine-app/src/search/SearchPage.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
+import iconImg from './ICON.png'
+import './search.css'
 
 export default function SearchPage() {
     return(
-        <h2>Wine Search</h2>
+        <div className={"SearchPage"}>
+            <div className={"Search"}>
+                <div className={"SearchFrame"}>
+                    <header className={"SearchHeader"}>
+                        Wine Search
+                    </header>
+                    <img src={iconImg} className={"Icon"}/>
+                </div>
+            </div>
+        </div>
+
     )
 
 }
